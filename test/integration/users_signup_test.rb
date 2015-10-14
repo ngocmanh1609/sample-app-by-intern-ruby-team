bd914efd8e655f042dcb4cc4a891518e600bb7d4
YOUR NAME <YOUR@EMAIL.com>
2015-10-13 22:11:40 -0400

    fix in test file

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,8 +11,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                password_confirmation: 'bar' }
     end
     assert_template 'users/new'
-    # assert_select 'div#<CSS id for error explanation>'
-    # assert_select 'div.<CSS class for field with error>'
   end
 
   test 'valid signup information' do
@@ -24,6 +22,5 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: 'password' }
     end
     assert_template 'users/show'
-    # assert_not flash.FILL_IN
   end
 end
