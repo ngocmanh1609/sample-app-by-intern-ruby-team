10c79ced206e305b2b16d9ee66d3965739648dbc
ngocmanh1609 <ngocmanh1609@gmail.com>
2015-10-21 16:45:22 +0700

    [add] access control for micropost controller

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,4 +1,10 @@
 # encoding: utf-8
 # Microposts controller
 class MicropostsController < ApplicationController
+  before_action :logged_in_user, only: [:create, :destroy]
+  def create
+  end
+
+  def destroy
+  end
 end
